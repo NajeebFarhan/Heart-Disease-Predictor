63a28a1be84763134045650875eb409f78de6b59
NajeebFarhan <najeebfarhan6345@gmail.com>
2026-01-01 14:22:46 +0530

    first commit after lots of coming

---
+++ heart_disease_predictor.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import joblib
+from typing import Any
+
+class HeartDiseasePredictor:
+    def __init__(self, model_path: str = None, threshold: float = 0.4) -> None:
+        self.model = joblib.load(model_path or "heart_disease_predictor.joblib")
+        self.threshold = threshold
+
+    def predict(self, data: dict[str, Any] | pd.Series) -> tuple[float, int]:
+        """
+        Returns (risk_probability, prediction)
+        """
+        df = pd.DataFrame([data])
+        risk_probability = float(self.model.predict_proba(df)[:, 1][0])
+        prediction = int(risk_probability >= self.threshold)
+
+        return risk_probability, prediction
